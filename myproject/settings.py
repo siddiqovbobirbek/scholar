77a29f366726a88f4a6a927fb63a9c4b9a4c365e
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-02-20 23:43:10 +0500

    maxfiy

--- myproject/settings.py
+++ myproject/settings.py
@@ -111,7 +111,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATICFILE_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+# STATICFILE_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
