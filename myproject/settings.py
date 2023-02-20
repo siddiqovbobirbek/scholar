ab01ad552ba9779d61e055f118f05661bf90de75
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-02-20 17:24:39 +0500

    salomcha

--- myproject/settings.py
+++ myproject/settings.py
@@ -109,6 +109,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATICFILE_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
