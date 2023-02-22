55ca9faef13cb174558394e4ac8c4155834de884
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-02-22 12:44:01 +0500

    commit9

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ env.read_env()
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
