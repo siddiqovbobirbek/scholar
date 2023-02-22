3e9c796dddd6129905699cc11760dcda98230e71
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-02-22 12:19:43 +0500

    commit6

--- Users/urls.py
+++ Users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from Users.views import register
 from Users.views import login
-from myApp.views import index
+from myApp.views import index, book, dissertation, maqola, dgu
 
 urlpatterns = [
     path('register/', register, name="register"),
     path('login/', login, name="login"),
     path('index/', index, name="index"),
+    path("dgu/", dgu, name="dgu"),
+    path("maqola/", maqola, name="maqola"),
+    path("book/", book, name="book"),
+    path("dissertation/", dissertation, name="dissertation"),
 ]
