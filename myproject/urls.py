0f2e3012428165f3a5b461d9d33a72a2af7d2d86
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-02-27 11:15:43 +0500

    ishlaydi

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("myApp.urls")),
     path('', include("Users.urls")),
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 
 ]
 
