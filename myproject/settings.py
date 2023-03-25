cb81b97a08bb756102aa8ec50be0fc1d25a3160c
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-03-25 18:15:04 +0500

    Edited database settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,13 +93,22 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': env.dj_db_url('DATABASE_URL', default='sqlite:///db.sqlite3')
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
+if env.str('SERVER', None) == 'timeweb':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env.str('DB_NAME'),
+            'USER': env.str('DB_USER'),
+            'PASSWORD': env.str('DB_PASSWORD'),
+            'HOST': env.str('DB_HOST'),
+            'PORT': env.str('DB_PORT'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': env.dj_db_url('DATABASE_URL', default='sqlite:///db.sqlite3')
+    }
+
 
 
 # Password validation
