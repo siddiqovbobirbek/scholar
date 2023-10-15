2819602a6647bacbad750edf3765cf6a03608a89
Komronbek Tufliyev <komronbek773@gmail.com>
2023-10-15 13:47:02 +0500

    hska

--- myApp/views.py
+++ myApp/views.py
@@ -243,12 +243,10 @@ def maqola(request):
                 volum=volum, issue=issue, sahifalar=sahifalar, keywords=key_words,
                 abstract=abstract, references=references
                 )
+                messages.success(request, f"Maqola yaratildi")
             except Exception as e:
                 print("Maqola yaratilmadi", e)
                 messages.error(request, f"Maqola yaratilmadi: {e}")
-            finally:
-                messages.success(request, f"Maqola yaratildi")
-                print("Maqola yaratildi")
             print("Maqola: ", maqola)
             url = reverse('myApp:upload_maqola', kwargs={'maqola_id': maqola.pk} )
         return redirect(url) 
