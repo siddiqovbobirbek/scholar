d853d36c44668683536edb208a2948b171cf3fb2
siddiqovbobirbek <siddiqovbobirbekman@gmail.com>
2023-10-17 11:32:07 +0500

    oka

--- myproject/settings_release.py
+++ myproject/settings_release.py
@@ -2,7 +2,7 @@ from .settings_base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
