85f7a91655c2e7227a4f234641d0ab1effb323b8
Komronbek Tufliyev <komronbek773@gmail.com>
2023-10-19 16:01:35 +0500

    reverted

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -10,10 +10,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 # from importlib import reload
 import os
 
-import sys
-# reload(sys)
-sys.setdefaultencoding('utf-8')
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
