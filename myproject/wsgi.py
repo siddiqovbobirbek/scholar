f3e1ced600f1b78b284d1914b711ab52f4639b30
Komronbek Tufliyev <komronbek773@gmail.com>
2023-10-19 15:57:29 +0500

    kja

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -7,7 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
+# from importlib import reload
 import os
+
+os.environ['LANG']='en_US.UTF-8'
+os.environ['LC_ALL']='en_US.UTF-8'
+import sys
+# reload(sys)
+sys.setdefaultencoding('utf-8')
 
 from django.core.wsgi import get_wsgi_application
 
